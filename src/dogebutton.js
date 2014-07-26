96c919abb1ffa81dc96041f86067424004c33e59
Felix Yadomi <dev.yadomi@gmail.com>
2014-07-26 15:51:39 +0200

    Fix: trigger hover when no flash available

--- src/dogebutton.js
+++ src/dogebutton.js
@@ -21,7 +21,6 @@ include('//cdnjs.cloudflare.com/ajax/libs/zeroclipboard/2.1.5/ZeroClipboard.min.
             event.clipboardData.setData('text/plain', event.target.dataset.address);
         } );
         clip.on( 'aftercopy', function(event) {
-            // event.traget.classList.add('copied');
             event.target.className += ' copied';
         });
     } );
