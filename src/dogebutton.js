deca179c0ea49f9fa1532fc414c32076d79a5152
Felix Yadomi <felix.yadomi@gmail.com>
2021-02-13 00:54:12 +0100

    Removes zeroclipboard and less, simplify/rewrite code

--- src/dogebutton.js
+++ src/dogebutton.js
@@ -1,73 +1,16 @@
-var DogeClassPrefix = 'xdgbtn-';
+const dogeClassPrefix = 'xdgbtn';
 
-var DogeContent = '<div class="' + DogeClassPrefix + 'symbol">Ð </div><p><span class="' + DogeClassPrefix + 'currency">Dogecoin</span></p>';
-var DogeButtons = [].slice.call(document.querySelectorAll('[data-xdgbtn-address]'));
+const withPrefix = className => `${dogeClassPrefix}-${className}`;
+const CLASSNAMES = ['opened', 'currency', 'symbol', 'donate', 'theme-corpo', 'theme-doge'].reduce((sum, className) => {
+    sum[className] = withPrefix(className)
+    return sum;
+}, {});
 
-DogeButtons.forEach(function(btn) {
-    btn.innerHTML = DogeContent;
-    if(hasClass(btn, prefixClass('donate'))){
-        getBalance(btn.getAttribute('data-xdgbtn-address'), function(data){
-            btn.querySelector('.' + prefixClass('symbol')).dataset.balance = Math.round(data).toFixed(2);
-        });
-    }
-});
-
-include('//cdnjs.cloudflare.com/ajax/libs/zeroclipboard/2.1.5/ZeroClipboard.min.js', function(){
-    ZeroClipboard.config( {
-        swfPath: "//cdnjs.cloudflare.com/ajax/libs/zeroclipboard/2.1.5/ZeroClipboard.swf" , 
-        hoverClass: 'isHover'
-    } );
-    var clip = new ZeroClipboard(DogeButtons);
-    clip.on( 'ready', function(event) {
-        clip.on( 'copy', function(event) {
-            event.clipboardData.setData('text/plain', event.target.dataset.address);
-        } );
-        clip.on( 'aftercopy', function(event) {
-            event.target.classList.add('copied')
-            setTimeout(function(){
-                event.target.classList.remove('copied')
-            }, 3100);
-        });
-    } );
-    clip.on( 'error', function(event) {
-        ZeroClipboard.destroy();
-    } );
-});
-
-function hasClass(el, classname) {
-    return ((" " + el.className + " ").replace(/[\n\t]/g, " ").indexOf(classname) > -1)
-}
-
-var closeButton = function() {
-    DogeButtons.forEach(function(btn) {
-        if (hasClass(btn, prefixClass('opened'))) {
-            btn.classList.remove(prefixClass('opened'));
-            btn.querySelector('.' + prefixClass('currency')).innerHTML = 'Dogecoin';
-        }
-    });
-}
-
-DogeButtons.forEach(function(button){
-    button.addEventListener('click', bindClickEvent);
-});
-
-function bindClickEvent(event) {
-    var btn = event.currentTarget;
-    if (hasClass(btn, prefixClass('opened'))) {
-        closeButton();
-    } else {
-        btn.classList.add(prefixClass('opened'));
-        btn.querySelector('.' + prefixClass('currency')).innerHTML = btn.getAttribute('data-xdgbtn-address');
-    }
-}
-
-function prefixClass(className) {
-    return DogeClassPrefix + className;
-}
+const template = (address) => `<section><div class="${CLASSNAMES.symbol}"><span></span></div><div><span class="${CLASSNAMES.currency}">Dogecoin</span></div></section><div><span></span><span>${address}</span></div>`;
 
 function getBalance(addr, callback){
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://dogechain.info/chain/Dogecoin/q/addressbalance/"+addr ,true);
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://dogechain.info/chain/Dogecoin/q/addressbalance/" + addr ,true);
     xhr.send();
     xhr.onreadystatechange =  function(){
         if ((xhr.readyState == 4) && (xhr.status == 200)) {
@@ -76,14 +19,34 @@ function getBalance(addr, callback){
     }
 }
 
-function include(path, callback){
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.src = path;
-    script.type = 'text/javascript';
+const onClick = ({ currentTarget }) => {
+    if (currentTarget.classList.contains(CLASSNAMES.opened)) {
+        currentTarget.classList.remove(CLASSNAMES.opened);
+        return;
+    }
 
-    script.onreadystatechange = callback;
-    script.onload = callback;
+    if ('clipboard' in navigator) {
+        navigator.clipboard.writeText(currentTarget.dataset.xdgbtnAddress)
+    }
 
-    head.appendChild(script)
+    currentTarget.classList.add(CLASSNAMES.opened);
 }
+
+(() => {
+    const dogeButtons = [].slice.call(document.querySelectorAll('[data-xdgbtn-address]'));
+
+    dogeButtons.forEach(function(btn) {
+        btn.innerHTML = template(btn.dataset.xdgbtnAddress);
+        btn.addEventListener('click', onClick);
+
+        if(![...btn.classList].some(name => [CLASSNAMES['theme-doge'], CLASSNAMES['theme-corpo']].includes(name))) {
+            btn.classList.add(CLASSNAMES['theme-corpo'])
+        }
+
+        if (btn.classList.contains(CLASSNAMES.donate)) {
+            getBalance(btn.dataset.xdgbtnAddress, function(data){
+                btn.querySelector('.' + CLASSNAMES.symbol).dataset.balance = Math.round(data).toFixed(2);
+            });
+        }
+    });
+})()
